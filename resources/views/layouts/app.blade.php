005293fe33680cad9bea696eccc36a8b4f82b3b4
mdraushan <mdraushanji22@gmail.com>
2026-03-23 22:58:05 +0530

    CRUD in Laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
     <!-- Footer -->
     <footer class="bg-light text-center text-lg-start mt-5">
         <div class="container p-4">
-            <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.2);">
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
                 <p>Home</p>
                 <p>About</p>
                 <p>Contact</p>
